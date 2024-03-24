e1f2500140a6c725cbfd0b93a030e485afe7bab4
Rick <ricleyreis@gmail.com>
2024-03-23 21:01:55 -0300

    readme update, criando api para preencher os produtos

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # JapaCasa! (3)
 
 ### Terceira versão reformulada da página na web para um restaurante de comida japonesa (fictício) focado em delivery.
-### Desenvolvida em HTML, CSS e JS.
+### Desenvolvida em HTML, CSS, JS, Node.js e Json-server.
+
+Utilizei uma api para preencher os produtos disponíveis no cardápio, a api está rodando no json-server e foi feita com Node.js.
 
 ## Créditos
 #### Para fazer esta versão, me ispirei no site do restaurante Ashin Sushi Bar (https://anshinsushibar.com.br) e também no site do restaurante Shogá Holl (https://www.shogaholl.com.br).
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,5 @@
 const japa = document.getElementById("japacasa");
 const spjapa = document.getElementById("spjapa");
-// const textH = document.getElementById("text-H");
-// const spjapa2 = document.getElementById("spjapa2");
 
 japa.addEventListener(
     "mouseover",
@@ -17,17 +15,51 @@ japa.addEventListener(
     }
 );
 
-// textH.addEventListener(
-//     "mouseover",
-//     (event) => {
-//         console.log("foi")
-//         spjapa2.style.border = "1px solid black";
-//     }
-// );
+async function fetchProdutos(){
+    try{
+        const response = await fetch('https://scarce-invented-basin.glitch.me/produtos');
+        const data = await response.json();
+        return data;
+    }catch(error){
+        console.error('Erro ao buscar os produtos', error);
+        return[];
+    }
+}
 
-// textH.addEventListener(
-//     "mouseout",
-//     (event) => {
-//         spjapa2.style.border = "0px solid black";
-//     }
-// );
+async function preencheProdutos(){
+    const produtos = await fetchProdutos();
+    const contentHomeC = document.querySelector('.content_homeC');
+
+    produtos.forEach(produto => {
+        const foodCard = document.createElement('div');
+        foodCard.classList.add('food-card');
+
+        const imagem = document.createElement('img');
+        imagem.src = produto.imagem;
+        foodCard.appendChild(imagem);
+
+        const nome = document.createElement('h2');
+        nome.textContent = produto.nome;
+        foodCard.appendChild(nome);
+
+        if(produto.quantidade){
+            const quantidade = document.createElement('h3');
+            quantidade.textContent = produto.quantidade;
+            foodCard.appendChild(quantidade);
+        }
+
+        if(produto.quantidadeDtd){
+            const quantidadeDtd = document.createElement('h5');
+            quantidadeDtd.textContent = produto.quantidadeDtd;
+            foodCard.appendChild(quantidadeDtd);
+        }
+
+        const preco = document.createElement('h4');
+        preco.textContent = produto.preco;
+        foodCard.appendChild(preco);
+
+        contentHomeC.appendChild(foodCard);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', preencheProdutos);
