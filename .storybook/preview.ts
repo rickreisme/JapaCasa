ecb29af22050888422b0699cb7ef731346b1f88a
Rick <ricleyreis@gmail.com>
2024-07-27 16:33:36 -0300

    FIX - configurando Stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from "@storybook/react";
+import {Decorator} from "@storybook/react"
+import '../src/assets/styles/global.scss'
+import '../src/assets/styles/menu.scss'
+
 
 const preview: Preview = {
     parameters: {
---
+++ src/components/Header.stories.tsx
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import '../assets/styles/menu.scss'
+
+export default {
+    title: 'Components/Header',
+    component: Header
+}
+
+export const Default ={}
